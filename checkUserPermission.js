af02fa9d259e7a55a300ec887aec2082b9413521
Mark Adams <mark@k4connect.com>
2022-05-27 17:10:34 -0400

    Update check permissions

---
+++ checkUserPermission.js
@@ -0,0 +1,17 @@
+const _ = require('lodash');
+const YAML = require('yaml');
+const FS = require('fs/promises');
+const Path = require('path');
+
+const { REQUIRED_PERMISSION, WORKING_DIRECTORY = '', USER } = process.env;
+
+async function checkUserPermission ({ github, context, core }) {
+	const permissionsFile = await FS.readFile(Path.join(WORKING_DIRECTORY, './permissions.yaml'));
+	const yaml = YAML.parse(permissionsFile.toString());
+	const users = _.get(yaml, REQUIRED_PERMISSION);
+	if ( users.indexOf(USER) === -1 ) {
+		core.setFailed(`User ${USER} does not have required permission '${REQUIRED_PERMISSION}'.`);
+	}
+}
+
+module.exports = checkUserPermission;
