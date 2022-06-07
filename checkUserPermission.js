54e356d63f9d1c625e61b883e07853b41838e264
Mark Adams <mark@k4connect.com>
2022-06-07 14:19:11 -0400

    Handle missing permission case

--- checkUserPermission.js
+++ checkUserPermission.js
@@ -9,7 +9,7 @@ async function checkUserPermission ({ github, context, core }) {
 	const permissionsFile = await FS.readFile(Path.join(WORKING_DIRECTORY, './permissions.yaml'));
 	const yaml = YAML.parse(permissionsFile.toString());
 	const users = _.get(yaml, REQUIRED_PERMISSION);
-	if ( users.indexOf(USER) === -1 ) {
+	if ( !users || users.indexOf(USER) === -1 ) {
 		core.setFailed(`User ${USER} does not have required permission '${REQUIRED_PERMISSION}'.`);
 	}
 }
